d17d42c22335d986c0a4bc4e7e5b9cee4c664997
Sergey <krash078@gmail.com>
2022-05-28 13:11:05 +0300

    end style gallery

--- js/select.js
+++ js/select.js
@@ -3,6 +3,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const element = document.querySelector('.select-js');
   const choices = new Choices(element, {
     searchEnabled: false,
+    itemSelectText: '',
+    shouldSort: false,
   });
 
 })
