88416b0b7ab6d5aaad1c462c8e4ded92f15adf9b
Sergey <krash078@gmail.com>
2022-06-02 19:11:22 +0300

    gallery modal js btn start

---
+++ js/gallery-modal.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  const btn = document.querySelectorAll('.gallery__slide');
+  const modalOverlay = document.querySelector('.gallery__modals-overlay');
+  const modalWindow = document.querySelectorAll('.gallery__modals-window');
+
+  btn.forEach(el => {
+    el.addEventListener('click', (e) => {
+      let path = e.currentTarget.getAttribute('data-path');
+
+      modalWindow.forEach(el => {
+        el.classList.remove('gallery__modals-window--visible')
+      });
+
+      document.querySelector(`[data-target="${path}"]`).classList.add('gallery__modals-window--visible');
+      modalOverlay.classList.add('gallery__modals-overlay--visible');
+    });
+  });
+
+  modalOverlay.addEventListener('click', (e) => {
+    if (e.target == modalOverlay) {
+      modalOverlay.classList.remove('gallery__modals-overlay--visible');
+      modalWindow.forEach(el => {
+        el.classList.remove('gallery__modals-window--visible');
+      });
+    }
+  });
+
+})
