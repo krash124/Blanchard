b995e4fbab4a49d9fcb62f84e00778bce7cfe6b2
Sergey <krash078@gmail.com>
2022-06-16 22:04:13 +0300

    accordion jquery

--- js/catalog-accordion.js
+++ js/catalog-accordion.js
@@ -1,8 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-  new Accordion('.accordion-js', {
-    duration: 400,
-    openOnInit: [0]
+  // new Accordion('.accordion-js', {
+  //   duration: 400,
+  //   openOnInit: [0]
+  // });
+
+
+
+  $( function() {
+    $( ".accordion-js" ).accordion({
+      icons: false,
+      heightStyle: "content",
+      collapsible: true,
+      active: 0,
+    });     
   });
+
 
 })
