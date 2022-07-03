225d0749f9b2d6191ab6096513846558f14e9abc
krash078 <krash078@users.noreply.github.com>
2022-07-03 18:16:43 +0300

    projects arrow

---
+++ js/projects-swiper.js
@@ -0,0 +1,64 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  const swiperProjects = new Swiper ('.projects__swiper', {
+    pagination: {
+      el: ".gallery__swiper-number",
+      type: "fraction"
+    },
+
+    navigation: {
+      nextEl: '.projects__swiper-next',
+      prevEl: '.projects__swiper-prev',
+    },
+
+    slidesPerView: 1,
+    grid: {
+      rows: 1,
+      fill: "row"
+    },
+    spaceBetween: 20,
+    breakpoints: {
+      441: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+      }
+    },
+
+    a11y: false,
+
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+
+    watchSlidesProgress: true, // added class swiper-slide-visible
+
+
+    on: {
+      init: function () {
+        this.slides.forEach(slide => {
+          if (!slide.classList.contains('swiper-slide-visible')) {
+            slide.tabIndex = '-1';
+          } else {
+            slide.tabIndex = '';
+          }
+        });
+      },
+      slideChange: function () {
+        this.slides.forEach(slide => {
+          if (!slide.classList.contains('swiper-slide-visible')) {
+            slide.tabIndex = '-1';
+          } else {
+            slide.tabIndex = '';
+          }
+        });
+      }
+    }
+  })
+
+})
