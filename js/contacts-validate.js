c274e312e5fbdb8c6d89453f240eec7a24ad1264
Sergey <krash078@gmail.com>
2022-07-10 17:52:22 +0300

    start map

--- js/contacts-validate.js
+++ js/contacts-validate.js
@@ -1,5 +1,56 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-  
+  // Input-Tel
+
+
+  var selector = document.querySelector("input[type='tel']");
+  var im = new Inputmask("+7 (999)-999-99-99");
+  im.mask(selector);
+
+  // Validate
+
+  const validation = new JustValidate('#contacts-validate');
+
+  validation
+
+  .addField('#name', [
+    {
+      rule: 'minLength',
+      value: 3,
+      errorMessage: 'Не менее 3-х сиволов',
+    },
+    {
+      rule: 'maxLength',
+      value: 30,
+      errorMessage: 'Не больше 30 символов',
+    },
+    {
+      rule: 'required',
+      errorMessage: 'Обязательно для заполнения',
+    },
+    {
+      rule: 'customRegexp',
+      value: /^[а-яА-ЯёЁa-zA-Z]+$/,
+      errorMessage: 'Недопустимый формат'
+    },
+  ])
+
+  .addField('#phone', [
+    {
+      rule: 'required',
+      errorMessage: 'Обязательно для заполнения',
+    },
+    {   
+      rule: "function",
+      validator: function (name, value) {
+        const phone = selector.inputmask.unmaskedvalue();
+        return phone.length === 10
+      },
+      errorMessage: 'Не достаточное количество символов',
+    },
+    
+  ])
+
+
 
 })
